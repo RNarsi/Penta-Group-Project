34b9576d82bcba66812270b80552f5255590eb99
RNarsi <narsiriya@gmail.com>
2024-04-24 11:10:05 +0200

    New Updates -Array script , UI stuff

---
+++ Penta/Assets/Scripts/Array.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    public GameObject[] BlackPieces;
+    public GameObject[] WhitePieces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
